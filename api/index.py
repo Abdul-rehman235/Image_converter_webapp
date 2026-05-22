6fd1c64a7bc5439ad2dcf6dff0ca3049e1d9d4ca
Abdul-rehman235 <ar0170245@gmail.com>
2026-05-22 17:36:12 +0500

    all done

--- api/index.py
+++ api/index.py
@@ -44,7 +44,9 @@ def tool():
 def login():
     return render_template('login.html')
 
-
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
