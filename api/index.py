fd1ec8228d36e51c6fe7072957f07a1d0e56e43f
Abdul-rehman235 <ar0170245@gmail.com>
2026-06-15 18:53:55 +0500

    add envor variable

--- api/index.py
+++ api/index.py
@@ -1,39 +1,41 @@
 from email.mime import image
 import base64, io
-
+from supabase import create_client, Client
+from datetime import datetime, timezone
 from flask import Flask, render_template, request, redirect, url_for, session
 from PIL import Image, ImageEnhance
 import os
+from dotenv import load_dotenv
 
-
-
+load_dotenv()
 # Flask ko batana ke templates aur static folders bahar root par hain
 app = Flask(__name__, 
             template_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '../templates')),
             static_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '../static')))
 
+url: str = os.getenv("URL")
+key: str = os.getenv("KEY")
+supabase: Client = create_client(url, key)
+
+
 app.secret_key = "my_secret_key"
 
-user_data = {
-    "user_ip": [],
-    "email": [],
-    "username": [],
-    "password": []
-}
+user_ip = [
 
+    ]
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     user = None
 
-    user_ip = request.remote_addr
-    user = user_ip in user_data['user_ip']
-    username = session.get('user') if user else None
-    email = session.get('email') if user else None
+    # user_ip = request.remote_addr
+    # user = user_ip in user_ip
+    username = supabase.table("user_data").select("username").eq("username", username).execute().data[0]['username'] if user else None
+    email = supabase.table("user_data").select("email").eq("email", email).execute().data[0]['email'] if user else None
 
 
-    return render_template('index.html', user=user, username=username, email=email)
+    return render_template('index.html', username=username, email=email)
 
 # this tool route
 @app.route('/tool', methods=['GET', 'POST'])
@@ -41,10 +43,10 @@ def tool():
     encoded_image = None  # Yeh HTML ko pass hoga
     user = None
 
-    user_ip = request.remote_addr
-    user = user_ip in user_data['user_ip']
-    username = session.get('user') if user else None
-    email = session.get('email') if user else None
+    # user_ip = request.remote_addr
+    # user = user_ip in user_ip
+    username = supabase.table("user_data").select("username").eq("username", username).execute().data[0]['username'] if user else None
+    email = supabase.table("user_data").select("email").eq("email", email).execute().data[0]['email'] if user else None
 
 
     if request.method == 'POST':
@@ -68,19 +70,19 @@ def tool():
             encoded_image = f"data:image/{ext};base64,{b64_string}"            
 
             
-    return render_template('tool.html', user_photo=encoded_image, user_ip=user_ip, user=user, username=username, email=email)
+    return render_template('tool.html', user_photo=encoded_image, username=username, email=email)
 
 
 
 @app.route('/compressor', methods=['GET', 'POST'])
 def compressor():
     encoded_image = None  # Yeh HTML ko pass hoga
-    user = None
+    # user = None
 
-    user_ip = request.remote_addr
-    user = user_ip in user_data['user_ip']
-    username = session.get('user') if user else None
-    email = session.get('email') if user else None
+    # user_ip = request.remote_addr
+    # user = supabase.table("users").select("*").eq("user_ip", user_ip).single().execute()
+    username = supabase.table("user_data").select("username").eq("username", username).execute()
+    email = supabase.table("user_data").select("email").eq("email", email).execute()
 
 
     if request.method == 'POST':
@@ -97,7 +99,7 @@ def compressor():
         # Data URL format jo HTML ka <img> tag samajhta hai
         encoded_image = f"data:image/{format};base64,{b64_string}"
 
-    return render_template('compressor.html', compressed_photo=encoded_image, user=user, username=username, email=email)
+    return render_template('compressor.html', compressed_photo=encoded_image, username=username, email=email)
 
 
 @app.route('/enhance', methods=['GET', 'POST'])
@@ -146,7 +148,9 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        if username in user_data['username'] and password in user_data['password']:
+        user_name = supabase.table("user_data").select("username").eq("username", username).execute()
+        user_pass = supabase.table("user_data").select("password").eq("password", password).execute()
+        if username in user_name and password in user_pass:
             # session['user'] = username
             # session['pass'] = password
             return redirect('/')
@@ -157,20 +161,26 @@ def login():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if request.method == 'POST':
+    if request.method == 'POST':  
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
-        user_ip = request.remote_addr
-        session['user'] = username
-        session['email'] = email
-        session['user_ip'] = user_ip
-        session['pass'] = password
-        if email not in user_data['email']:
-            user_data['username'].append(username)
-            user_data['email'].append(email)
-            user_data['password'].append(password)
-            user_data['user_ip'].append(user_ip)
+        # user_ip = request.remote_addr
+        check = supabase.table("user_data").select("email").eq("email", email).execute()
+        if email not in check:
+            try:
+               data = {
+                   "username": username,
+                   "email": email,
+                   "password": password,
+                   "account_date": datetime.now(timezone.utc).isoformat()
+               }
+               
+               response = supabase.table("user_data").insert(data).execute()
+               print("Data inserted successfully:", response)
+           
+            except Exception as e:
+                print("Error inserting data:", e)
             return redirect('/')
         else:
             return redirect(url_for('login'))
